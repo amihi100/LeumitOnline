ff738999b0a4ab8f9c03744b42d188871bf637b1
amihi100 <amihi100@gmail.com>
2025-05-04 17:23:14 +0300

    Add .gitignore file to exclude target/ and logs/ folders

--- target/cucumber-reports/web-timeline/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"636929ce-b304-4cd7-8acd-399616dd6c81","feature":"Basic Web Server Tests","scenario":"Performance testing","start":1746362735216,"group":38,"content":"","tags":"@web,@performance,","end":1746362738841,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":38,"content":"Thread[#38,TestNG-PoolService-0,5,main]"}]);
-});
--- target/test-classes/features/mobile/basic_mobile_launch.feature
+++
@@ -1,8 +0,0 @@
-Feature: Launch and Validate App UI
-
-  @mobile @launch
-  Scenario: App launch validation
-    Given I open the "leumit.mobile" app
-    Then The app should be fully loaded
-    And I close the app
-    Then Assert app is closed 
